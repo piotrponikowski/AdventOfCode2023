61103a6ead4e94d8f0a4149bb7449ccbc6bb2c16
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2023-12-02 12:26:17 +0100

    day 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,15 +1,58 @@
-class Day2(input: String) {
-    
+import kotlin.math.max
 
-    fun part1() = 1
+class Day2(val input: List<String>) {
 
-    fun part2() = 2
+
+    fun part1() {
+        val test = parse()
+        val test2 = test.filter { it.isPossible() }.map { it.id }.sum()
+        println(test2)
+    }
+
+    fun part2() {
+        val test = parse()
+        val test2 = test.map { it.power() }
+        println(test2.sum())
+    }
+
+    fun parse() = input.map { line ->
+        val game = line.split(":").first()
+        val cubes = line.split(":").last().split(";")
+
+        val id = game.split(" ").last().toInt()
+
+        val cubes2 = cubes.map { group ->
+            group.split(",").map { it.trim() }
+                .map { it.split(" ") }
+                .map { (a, b) -> Cubes(b, a.toInt()) }
+        }
+
+        Game(id, cubes2)
+    }
+
+
+    data class Game(val id: Int, val cubes: List<List<Cubes>>) {
+
+        fun isPossible() = cubes.all { a -> a.all { b -> colors[b.color]!! >= b.count } }
+
+        fun power():Int {
+            val maxes = mutableMapOf<String, Int>()
+            cubes.forEach { a -> a.forEach { b -> maxes[b.color] = max((maxes[b.color] ?: 0), b.count) } }
+            return maxes.values.reduce { a, b -> a*b}
+        }
+
+        companion object {
+            val colors = mapOf("red" to 12, "green" to 13, "blue" to 14)
+        }
+    }
+
+    data class Cubes(val color: String, val count: Int)
 }
 
 fun main() {
-    val input = readText("day1.txt", true)
-    //val input = readLines("day1.txt", true)
+//    val input = readText("day2.txt", true)
+    val input = readLines("day2.txt")
 
-    val result = Day2(input).part1()
+    val result = Day2(input).part2()
     println(result)
 }

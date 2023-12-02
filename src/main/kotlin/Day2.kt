164d502faa0f8ecc552036d3fd0617b7e361e035
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2023-12-02 13:03:37 +0100

    cleanup

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -6,11 +6,7 @@ class Day2(input: List<String>) {
 
     fun part1() = games.filter { game -> game.isPossible() }.sumOf { game -> game.id }
 
-    fun part2() = games.sumOf { game ->
-        game.minCubes()
-            .map { (_, count) -> count }
-            .reduce { result, count -> result * count }
-    }
+    fun part2() = games.sumOf { game -> game.power() }
 
     private fun parseGames(input: List<String>) = input.map { line ->
         val (gameInput, subGamesInput) = line.split(":")
@@ -29,17 +25,11 @@ class Day2(input: List<String>) {
     data class Game(val id: Int, val subGames: List<SubGame>) {
         fun isPossible() = subGames.all { subGame -> subGame.isPossible() }
 
-        fun minCubes(): Map<String, Int> {
-            val result = mutableMapOf<String, Int>()
-            subGames.forEach { subGame ->
-                subGame.cubes.forEach { cube ->
-                    result[cube.color] = max(result[cube.color] ?: 0, cube.count)
-
-                }
-            }
-
-            return result
-        }
+        fun power() = subGames
+            .flatMap { subGame -> subGame.cubes }
+            .groupBy { cube -> cube.color }
+            .map { (_, cubes) -> cubes.maxOfOrNull { cube -> cube.count }!! }
+            .reduce { result, count -> result * count }
     }
 
     data class SubGame(val cubes: List<Cube>) {
@@ -54,12 +44,4 @@ class Day2(input: List<String>) {
             private val MAX_CUBES = mapOf("red" to 12, "green" to 13, "blue" to 14)
         }
     }
-}
-
-fun main() {
-//    val input = readText("day2.txt", true)
-    val input = readLines("day2.txt", true)
-
-    val result = Day2(input).part1()
-    println(result)
 }

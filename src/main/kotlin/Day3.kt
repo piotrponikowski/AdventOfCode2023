386378d696212826b6f57a3c6a73b76aae8e6ac4
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2023-12-03 10:32:02 +0100

    cleanup

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,102 +1,63 @@
-class Day3(val input: List<String>) {
+class Day3(input: List<String>) {
 
     private val board = input
         .flatMapIndexed { y, line -> line.mapIndexed { x, symbol -> Point(x, y) to symbol } }
         .filter { (_, symbol) -> symbol != '.' }
         .toMap()
 
-    val directions = (-1..1).flatMap { x -> (-1..1).map { y -> Point(x, y) } }
-        .filter { !(it.x == 0 && it.y == 0) }
+    private val gears = board.filter { (_, symbol) -> symbol == '*' }.keys
 
-    fun part1():Int {
-        val groups = parse()
-        return groups.filter { it.partNumber }.map { it.getNumber(board) }.sum()
-    }
+    private val neighbours = (-1..1).flatMap { x -> (-1..1).map { y -> Point(x, y) } } - Point(0, 0)
 
-    fun part2() :Int {
-        val groups = parse()
-        groups.forEach { group -> println("${group.getNumber(board)} ${group.stars}") }
-        
-        val stars = groups.filter { it.stars.isNotEmpty() }
-            .flatMap { it.stars }.toSet()
+    private val groups = parseGroups()
 
-        val values = stars.map { star -> 
-            val n = groups.filter { it.stars.contains(star) } 
-            if(n.size == 2) {
-                n[0].getNumber(board) *n[1].getNumber(board)
-            } else {
-                0
-            }
-        }
-        
-//        val test1 = stars.map { star -> groups.filter { b -> b.symbols.contains(star) } }
-        
-        return values.sum()
-    }
+    fun part1() = groups
+        .filter { group -> group.isPart() }
+        .sumOf { group -> groupToNumber(group) }
 
-    fun parse(): List<Group> {
-        val notGrouped = board.keys.filter { board[it]!!.isDigit() }.toMutableList()
+    fun part2() = gears
+        .map { gear -> groups.filter { group -> gear in group.symbols } }
+        .filter { groups -> groups.size == 2 }
+        .sumOf { (group1, group2) -> groupToNumber(group1) * groupToNumber(group2) }
+
+    private fun parseGroups(): List<Group> {
+        val notGroupedDigits = board.filter { (_, symbol) -> symbol.isDigit() }.keys.toMutableList()
         val groups = mutableListOf<Group>()
 
-        while (notGrouped.isNotEmpty()) {
-            val start = notGrouped.removeFirst()
-            val group = mutableSetOf(start)
-            val stars = mutableSetOf<Point>()
-            var partNumber = false
+        while (notGroupedDigits.isNotEmpty()) {
+            val pointsToCheck = mutableListOf(notGroupedDigits.removeFirst())
+            val groupedPoints = mutableSetOf<Point>()
+            val adjacentSymbols = mutableSetOf<Point>()
 
-            val nextToCheck = mutableListOf(start)
-            while (nextToCheck.isNotEmpty()) {
-                val next = nextToCheck.removeFirst()
-                group += next
+            while (pointsToCheck.isNotEmpty()) {
+                val point = pointsToCheck.removeFirst()
+                val symbol = board[point] ?: continue
 
-                val neighbours = directions.map { d -> next + d }
-                neighbours.forEach { neighbour ->
-                    val symbol = board[neighbour]
-                    if (symbol != null) {
-                        if (symbol.isDigit()) {
-                            if (neighbour !in group) {
-                                nextToCheck += neighbour
-                                notGrouped -= neighbour
-                            }
-                        } else {
-                            partNumber = true
-                            if(symbol == '*') {
-                                stars += neighbour
-                            }
-                        }
-                    }
+                if (symbol.isDigit() && point !in groupedPoints) {
+                    groupedPoints += point
+                    pointsToCheck += neighbours.map { neighbour -> point + neighbour }
+                } else if (!symbol.isDigit()) {
+                    adjacentSymbols += point
                 }
             }
 
-            groups += Group(group.toList(), stars, partNumber)
+            groups += Group(groupedPoints, adjacentSymbols)
+            notGroupedDigits -= groupedPoints
         }
-
         return groups
     }
 
-    data class Group(val points: List<Point>, val stars:Set<Point>, val partNumber: Boolean) {
+    private fun groupToNumber(group: Group) = group.points
+        .sortedBy { point -> point.x }
+        .map { point -> board[point]!! }
+        .fold("") { result, current -> result + current }
+        .toInt()
 
-        fun getNumber(board: Map<Point, Char>): Int {
-            return points.sortedBy { p -> p.x }
-                .map { p -> board[p]!! }
-                .fold("") { a, b -> (a + b.toString()) }
-                .toInt()
-        }
+    data class Group(val points: Set<Point>, val symbols: Set<Point>) {
+        fun isPart() = symbols.isNotEmpty()
     }
 
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Point) = Point(x + other.x, y + other.y)
     }
-
-    enum class Direction(val x: Int, val y: Int) {
-        L(-1, 0), R(1, 0), U(0, -1), D(0, 1)
-    }
-}
-
-fun main() {
-//    val input = readText("day3.txt", true)
-    val input = readLines("day3.txt" )
-
-    val result = Day3(input).part2()
-    println(result)
 }

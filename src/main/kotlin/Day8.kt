9bb9c89a706149f338be5b273811c4dee581d171
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2023-12-08 06:26:33 +0100

    day 8

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -3,13 +3,28 @@ class Day8(input: List<String>) {
     private val instructions = parseInstructions(input)
     private val nodes = parseNodes(input)
 
-    fun part1(start: String = "AAA"):Int {
+    fun part1() = solve("AAA") { current -> current != "ZZZ" }
+
+    fun part2():Long {
+        val staringNodes = nodes.filter { it.start.endsWith("A") }.map { it.start }
+        println(staringNodes)
+
+        val indices = staringNodes.map { solve(it) { current -> !current.endsWith("Z") } }.map { it.toLong() }
+        println(indices)
+
+        val result = indices.reduce { acc, l -> lcm(acc, l) }
+        println(result)
+        return result
+    }
+
+
+    fun solve(start: String = "AAA", endCondition: (String) -> Boolean): Int {
         var current = start
         var index = 0
-        while (!current.endsWith("Z")) {
+        while (endCondition(current)) {
             val instruction = instructions[index % instructions.size]
             val node = nodes.find { it.start == current }!!
-            if(instruction == 'L') {
+            if (instruction == 'L') {
                 current = node.left
             } else {
                 current = node.right
@@ -20,16 +35,6 @@ class Day8(input: List<String>) {
         return index
     }
 
-    fun part2() {
-        val staringNodes = nodes.filter { it.start.endsWith("A") }.map { it.start }
-        println(staringNodes)
-        
-        val indices = staringNodes.map { part1(it) }.map { it.toLong() }
-        println(indices)
-        
-        val result = indices.reduce { acc, l ->  lcm(acc, l)}
-        println(result)
-    }
 
     private fun parseInstructions(input: List<String>) = input.first().toList()
     private fun parseNodes(input: List<String>) = input.drop(2).map { line -> Node.parse(line) }
---
+++ src/test/kotlin/Day8Test.kt
@@ -0,0 +1,36 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class Day8Test : FunSpec({
+
+    val realInput = readLines("day8.txt")
+    val exampleInput1 = readLines("day8-1.txt", true)
+    val exampleInput2 = readLines("day8-2.txt", true)
+    val exampleInput3 = readLines("day8-3.txt", true)
+
+
+    context("Part 1") {
+        test("should solve example 1") {
+            Day8(exampleInput1).part1() shouldBe 2
+        }
+
+        test("should solve example 2") {
+            Day8(exampleInput2).part1() shouldBe 6
+        }
+
+
+        test("should solve real input") {
+            Day8(realInput).part1() shouldBe 16043
+        }
+    }
+
+    context("Part 2") {
+        test("should solve example") {
+            Day8(exampleInput3).part2() shouldBe 6
+        }
+        
+        test("should solve real input") {
+            Day8(realInput).part2() shouldBe 15726453850399
+        }
+    }
+})

344a91c54da550773afd73257406b484d8280430
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2023-12-09 06:23:52 +0100

    day 9

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,47 +1,33 @@
 class Day9(input: List<String>) {
 
-    val histories= input.map { it.split(" ").map { it.toInt() } }.map { History(it) }
-    
-    fun part1() {
-        val test = histories.map { solve(it) }
-        println(test.sum())
-    }
+    private val histories = input.map { it.split(" ").map { it.toInt() } }.map { History(it) }
 
-    fun part2() {
-        val test = histories.map { it.reversed() }.map { solve(it) }
-        println(test.sum())
-    }
-    
-    fun solve(history: History):Int {
-        val result = mutableListOf(history)
-        var current = history
-        
-        while (!current.allZeroes()) {
-            val next = current.diffs()
-            
-            result += next
-            current = next
-        }
-        
-        val test = result.reversed().fold(0) { a, b -> b.numbers.last() + a}
-        println()
-        return test
-    }
-    
-    data class History(val numbers:List<Int>) {
+    fun part1() = histories.sumOf { history -> history.nextDiff() }
+
+    fun part2() = histories.sumOf { history -> history.reversed().nextDiff() }
+
+
+    data class History(val numbers: List<Int>) {
         fun reversed() = History(numbers.reversed())
-        
-        fun diffs() = History(numbers.windowed(2).map { (a, b) -> b-a })
-        
-        fun allZeroes() = numbers.all { it == 0 }
-        
+
+        private fun diff() = numbers.windowed(2)
+            .map { (number1, number2) -> number2 - number1 }
+            .let { numbers -> History(numbers) }
+
+        private fun isEnd() = numbers.all { number -> number == 0 }
+
+        fun nextDiff(): Int {
+            val diffs = mutableListOf(this)
+            var current = this
+
+            while (!current.isEnd()) {
+                val nextDiff = current.diff()
+
+                diffs += nextDiff
+                current = nextDiff
+            }
+
+            return diffs.reversed().fold(0) { result, number -> number.numbers.last() + result }
+        }
     }
-}
-
-fun main() {
-//    val input = readText("day9.txt", true)
-    val input = readLines("day9.txt")
-
-    val result = Day9(input).part2()
-    println(result)
 }

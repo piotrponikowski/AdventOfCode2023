c9edc2de14f558762cae38c10a0575d5444dda30
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2023-12-13 17:15:03 +0100

    day 13

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -5,20 +5,22 @@ class Day13(input: String) {
     private val boards = groupLines(input).map { lines -> parseBoard(lines) }
 
     fun part1() = scoreMirrors(0)
-    
+
     fun part2() = scoreMirrors(1)
 
-    private fun scoreMirrors(allowedErrors:Int) = boards
+    private fun scoreMirrors(allowedErrors: Int) = boards
         .sumOf { mirror -> findMirror(mirror.transpose(), allowedErrors) * 100 + findMirror(mirror, allowedErrors) }
 
-    private fun findMirror(board: Board, allowedErrors:Int): Int {
+    private fun findMirror(board: Board, allowedErrors: Int): Int {
         val maxX = board.rocks.keys.maxOf { point -> point.x }
         val maxY = board.rocks.keys.maxOf { point -> point.y }
 
         val mirror = (0..<maxX).find { mirrorX ->
             val commonWidth = min(mirrorX, maxX - mirrorX - 1)
+
             val incorrectMatches = (0..maxY).sumOf { y ->
                 (0..commonWidth).count { distanceFromMirror ->
+                    
                     val leftPoint = Point(mirrorX - distanceFromMirror, y)
                     val rightPoint = Point(mirrorX + distanceFromMirror + 1, y)
 

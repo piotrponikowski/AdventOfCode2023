5ad0460c7290eb6290e4b2da64d048aed21115d1
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2023-12-15 08:52:11 +0100

    day 15

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,78 +1,49 @@
-class Day15(input: List<String>) {
+class Day15(input: String) {
 
-    val messages = input.first().split(",")
-    
-    fun part1() {
-        val a = messages.map { box(it) }.sum()
-        println(a)
-    }
-  
-    fun part2() :Int{
-        val registers = mutableMapOf<Int, List<Lens>>()
-        messages.forEach { line ->
-            val label = split(line)
-            val boxId = box(label)
-            
-            if(line.last() == '-') {
-                val lenses = registers[boxId] ?: listOf()
-                val newLenses = lenses.filter { it.label != label }
-                registers[boxId] = newLenses
+    private val messages = input.split(",").map { message -> Message.parse(message) }
+
+    fun part1() = messages.sumOf { message -> hash(message.data) }
+
+    fun part2(): Int {
+        val boxes = mutableMapOf<Int, List<Message>>()
+        
+        messages.forEach { message ->
+
+            val label = message.label
+            val boxId = hash(message.label)
+            val box = boxes[boxId] ?: emptyList()
+            val exists = box.any { content -> content.label == label }
+
+            if (message.value == null) {
+                boxes[boxId] = box.filter { content -> content.label != label }
+            } else if (exists) {
+                boxes[boxId] = box.map { content -> if (content.label == label) message else content }
             } else {
-                val lenses = registers[boxId] ?: listOf()
-                val containLenses = lenses.any { it.label == label }
-                if(containLenses) {
-                    val newLenses = lenses.map { if(it.label == label) Lens(it.label, line.split("=").last().toInt()) else it }
-                    registers[boxId] = newLenses
-                } else {
-                    val newLenses = lenses + Lens(label, line.split("=").last().toInt())
-                    registers[boxId] = newLenses  
-                }
-       
+                boxes[boxId] = box + message
             }
 
-            println()
-            println(line)
-            println(registers)
         }
-        
-        
-        val result = registers.map { (boxId, lenses) ->
-            lenses.mapIndexed { index, lens -> 
-                val a = boxId + 1
-                val b = index + 1
-                val c = lens.value
-                a * b * c
-            }.sum()
+
+        return boxes.map { (boxId, content) ->
+            content.mapIndexed { index, message -> (boxId + 1) * (index + 1) * (message.value!!) }.sum()
         }.sum()
-      return result
     }
-    
-    private fun split(input: String):String {
-        if(input.last() == '-') {
-            return input.dropLast(1)
-        } else {
-            return input.split("=").first() 
+
+    private fun hash(input: String) = input.toList()
+        .fold(0) { result, symbol -> ((result + symbol.code) * 17) % 256 }
+
+
+    data class Message(val data: String, val label: String, val value: Int? = null) {
+
+        companion object {
+            fun parse(input: String): Message {
+                val (label, value) = input.split('-', '=')
+                return if (value.isNotBlank()) {
+                    Message(input, label, value.toInt())
+                } else {
+                    Message(input, label)
+                }
+            }
         }
     }
-    
-    private fun box(input:String):Int {
-        var result = 0
-        input.toList().forEach { symbol ->
-            result += symbol.code
-            result *= 17
-            result %= 256
-        }
-        return result
-    }
-    
-    data class Lens(val label:String, val value: Int)
-
-}
-
-fun main() {
-//    val input = readText("day15.txt", true)
-    val input = readLines("day15.txt")
-
-    val result = Day15(input).part2()
-    println(result)
 }

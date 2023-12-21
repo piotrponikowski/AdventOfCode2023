130e51c2227a6d8a49327381a0f729e8e1036002
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2023-12-21 17:57:44 +0100

    day 21

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,21 +1,16 @@
+import kotlin.math.abs
 import kotlin.math.max
 
 class Day21(input: List<String>) {
 
     private val board = input
-        .flatMapIndexed { y, line -> line.mapIndexed { x, symbol -> Point(x, y) to symbol } }.toMap()
+        .flatMapIndexed { y, line -> line.mapIndexed { x, symbol -> Point(x.toLong(), y.toLong()) to symbol } }.toMap()
 
-    private val board2 = board.mapValues { (_, symbol) ->
-        when (symbol) {
-            '#' -> -1L
-            'S' -> 1L
-            else -> 0L
-        }
-    }
+    private val steps = board.keys.filter { point -> board[point]!! == 'S' }
 
     fun part1() {
         var state = board
-        (1..6).forEach {
+        (1..10).forEach {
             state = step(state)
             println()
             //printBoard(state)
@@ -26,50 +21,69 @@ class Day21(input: List<String>) {
     }
 
     fun part2() {
-        var state = board2
-        (1..10).forEach {
-            state = step2(state)
-            println()
-            printBoard2(state)
+//        var state = steps.toSet()
+//        val counts = mutableListOf<Int>()
+//        val width = board.keys.maxOf { it.x } + 1
+//
+//        (0..width * 3 + 65).forEach { step ->
+//            state = step2(state)
+//            counts += state.count()
+//            println("$step -> ${state.count()}")
+//        }
+//
+//        val counts2 = counts.filterIndexed { index, count -> (index+1) % width.toInt() == 65 }
+//        val diffs1 = counts2.windowed(2).map { (a, b) -> b - a }
+//        val diffs2 = diffs1.windowed(2).map { (a, b) -> b - a }
+//
+//        println(counts2)
+//        println(diffs1)
+//        println(diffs2)
+
+        var result = 186885L
+        var stepsCounted = 65 + 131+131+131
+        val stepsTotal = 26501365
+        val loopSize = 131
+
+        var delta1 = 91443L
+        var delta2 = 30442L
+
+        //622920842019122
+        //622927000436128
+        //622933158883576
+        //622934175946782
+        while (stepsCounted < stepsTotal) {
+            delta1 += delta2
+            result += delta1
+            stepsCounted += loopSize
         }
 
-        val count = state.values.filter { it > 0L }.sumOf { it }
-        println(count)
+        println(delta1)
+        println(result)
     }
 
 
-    private fun step2(state: Map<Point, Long>): Map<Point, Long> {
-        val maxX = state.keys.maxOf { it.x }
-        val maxY = state.keys.maxOf { it.y }
+    private fun step2(state: Set<Point>): Set<Point> {
+        val width = board.keys.maxOf { it.x } + 1
+        val height = board.keys.maxOf { it.y } + 1
 
-        return state.mapValues { (point, count) ->
-            if (count == -1L) {
-                count
-            } else {
-                val neighbours = Direction.entries
-                    .map { direction -> point + direction }
+        val newState = mutableSetOf<Point>()
+        state.map { point ->
 
-                val baseAny = neighbours
-                    .mapNotNull { neighbour -> state[neighbour] }
-                    .any { it > 0 }
+            val neighbours = Direction.entries
+                .map { direction -> point + direction }
+                .filter { neighbour ->
+                    val wx = ((neighbour.x % width) + width) % width;
+                    val wy = ((neighbour.y % height) + height) % height;
+                    val warpPoint = Point(wx, wy)
+                    val warpSymbol = board[warpPoint]!!
 
+                    warpSymbol == '.' || warpSymbol == 'S'
+                }
 
-                val wrappedCount = neighbours.map { neighbour ->
-                    when {
-                        neighbour.x < 0 -> Point(maxX, neighbour.y)
-                        neighbour.x > maxX -> Point(0, neighbour.y)
-                        neighbour.y < 0 -> Point(neighbour.x, maxY)
-                        neighbour.y > maxY -> Point(neighbour.x, 0)
-                        else -> null
-                    }
-                }.mapNotNull { neighbour -> state[neighbour] }.sumOf { if (it > 0L) it else 0L }
-
-
-                val baseCount = if (baseAny) 1L else 0L
-
-                baseCount+ wrappedCount
-            }
+            newState += neighbours
         }
+
+        return newState
     }
 
     private fun step(state: Map<Point, Char>): Map<Point, Char> {
@@ -119,18 +133,18 @@ class Day21(input: List<String>) {
         }
     }
 
-    data class Point(val x: Int, val y: Int) {
+    data class Point(val x: Long, val y: Long) {
         operator fun plus(other: Direction) = Point(x + other.x, y + other.y)
     }
 
-    enum class Direction(val x: Int, val y: Int) {
+    enum class Direction(val x: Long, val y: Long) {
         L(-1, 0), R(1, 0), U(0, -1), D(0, 1)
     }
 }
 
 fun main() {
 //    val input = readText("day21.txt", true)
-    val input = readLines("day21.txt", true)
+    val input = readLines("day21.txt")
 
     val result = Day21(input).part2()
     println(result)

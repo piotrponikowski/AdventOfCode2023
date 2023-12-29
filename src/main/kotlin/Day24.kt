3b6bf587f2574ca8a2c5c7e20bd68fc92e2edf6f
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2023-12-29 10:35:31 +0100

    day 24

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -1,27 +1,62 @@
+import kotlin.math.abs
+
 class Day24(input: List<String>) {
 
     private val hailstones = input.map { line -> Hailstone.parse(line) }
     private val maxX = hailstones.maxOf { hailstone -> hailstone.position.x }
-    
-    private val collisionRange = if(maxX < 100) 7L..27L else 200000000000000L..400000000000000L
+
+    private val collisionRange = if (maxX < 100) 7L..27L else 200000000000000L..400000000000000L
 
     fun part1(): Int {
         var counter = 0
-      
+
         hailstones.forEachIndexed { index1, hailstone1 ->
             hailstones.forEachIndexed { index2, hailstone2 ->
                 if (index2 > index1) {
-                    if (checkCollision(hailstone1, hailstone2, collisionRange)) {
+                    val collision = findCollision(hailstone1, hailstone2)
+                    if (collision != null && inCollisionRange(collision)) {
                         counter += 1
                     }
                 }
             }
         }
-        
+
         return counter
     }
 
-    private fun checkCollision(h1: Hailstone, h2: Hailstone, collisionRange: LongRange): Boolean {
+    fun part2() {
+        val scanRange = (-500L..500L)
+
+        scanRange.forEach { x ->
+            scanRange.forEach { y ->
+                val velocity = Point(x, y, 0)
+                val result = validateVelocity(velocity)
+                if(result) {
+                    println("$x, $y")
+                }
+            }
+        }
+    }
+
+    private fun validateVelocity(velocity: Point): Boolean {
+        val (h1, h2, h3) = hailstones
+
+        val hd1 = Hailstone(h1.position, h1.velocity - velocity)
+        val hd2 = Hailstone(h2.position, h2.velocity - velocity)
+        val hd3 = Hailstone(h3.position, h3.velocity - velocity)
+
+        val c1 = findCollision(hd1, hd2)
+        val c2 = findCollision(hd1, hd3)
+        val c3 = findCollision(hd2, hd3)
+
+        if (c1 != null && c2 != null && c3 != null) {
+            return c1.matches(c2) && c1.matches(c3) && c2.matches(c3)
+        } else {
+            return false
+        }
+    }
+
+    private fun findCollision(h1: Hailstone, h2: Hailstone): Collision? {
         val p1 = h1.position
         val p2 = h2.position
 
@@ -29,8 +64,9 @@ class Day24(input: List<String>) {
         val v2 = h2.velocity
 
         val denominator = (v1.y * v2.x) - (v1.x * v2.y)
+
         if (denominator == 0L) {
-            return false
+            return null
         }
 
         val tNumerator = v2.y * (p1.x - p2.x) - v2.x * (p1.y - p2.y)
@@ -40,19 +76,20 @@ class Day24(input: List<String>) {
         val s = sNumerator.toDouble() / denominator.toDouble()
 
         if (t < 0 || s < 0) {
-            return false
+            return null
         }
 
         val collisionX = p1.x + v1.x * t
         val collisionY = p1.y + v1.y * t
 
-        val validX = collisionX >= collisionRange.first && collisionX <= collisionRange.last
-        val validY = collisionY >= collisionRange.first && collisionY <= collisionRange.last
-
-        return validX && validY
+        return Collision(collisionX, collisionY)
     }
 
-    fun part2() = 2
+    private fun inCollisionRange(collision: Collision): Boolean {
+        val validX = collision.x >= collisionRange.first && collision.x <= collisionRange.last
+        val validY = collision.y >= collisionRange.first && collision.y <= collisionRange.last
+        return validX && validY
+    }
 
     data class Hailstone(val position: Point, val velocity: Point) {
 
@@ -72,6 +109,16 @@ class Day24(input: List<String>) {
     }
 
     data class Point(val x: Long, val y: Long, val z: Long) {
-        operator fun plus(other: Point) = Point(x + other.x, y + other.y, z + other.z)
+        operator fun minus(other: Point) = Point(x - other.x, y - other.y, z - other.z)
+    }
+
+    data class Collision(val x: Double, val y: Double) {
+        fun matches(other: Collision): Boolean {
+            return abs(x - other.x) < EPSILON && abs(y - other.y) < EPSILON
+        }
+
+        companion object {
+            const val EPSILON = 0.01
+        }
     }
 }
